a2703fb03d01c329f4f722e656607917570be850
Your Name <you@example.com>
2017-11-12 21:24:32 -0800

    game logic coding in progress not final yet

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -35,33 +35,62 @@ var wordList = ["Super Mario Bros",
 								"Resident Evil",
 								"Diablo"];
 
-var guessInProgress = false;
-var chosenWord;
+var isGuessInProgress = false;
+var currentWord;
 var wordGuessProgress = [];
+var numberOfGuessesRemaining = 15;
+var totalCorrect = 0;
+var totalToWin = 0;
+
 var wordToGuessDiv;
-var userGuessesDiv;
+var userGuessListDiv;
+var guessesRemainingDiv;
+var winOrLoseDiv;
 
 function playGame() {
 	wordToGuessDiv = document.getElementById('word-to-guess');
-	userGuessesDiv = document.getElementById('user-guesses');
+	userGuessListDiv = document.getElementById('user-guesses');
+	guessesRemainingDiv = document.getElementById("remaining-guesses");
+	winOrLoseDiv = document.getElementById("win-or-lose");
 
 	document.onkeyup = function(event) {
 		var userGuess = event.key;
 
 		if(userGuess !== 'Meta') {
-			if(guessInProgress) {
+			if(isGuessInProgress) {
+				checkUserGuess(userGuess);
 
+				displayWordGuessProgress();
+
+				updateUserGuesses(userGuess);
+
+				//updateNumberOfGuessesRemaining(false);
+
+				if(isWinner()) {
+					// Debug only
+					console.log('isWinner is true');
+
+					updateElementTextContent(winOrLoseDiv, 'YOU WIN!!!', false);
+
+					isGuessInProgress = false;
+				} else if(!moreGuessesAllowed()) {
+						updateElementTextContent(winOrLoseDiv, 'YOU LOSE!', false);
+						
+						isGuessInProgress = false;
+				} else {
+					isGuessInProgress = true;
+				}
 			}
 			else {
+				//updateElementTextContent(winOrLoseDiv, '', false);
+
 				clearUserGuesses();
 
 				setupNewWord();
 
 				displayWordGuessProgress();
 
-				updateUserGuesses(userGuess);
-
-				//guessInProgress = true;
+				isGuessInProgress = true;
 			}
 		}
 	};
@@ -71,20 +100,26 @@ function playGame() {
 
 function setupNewWord() {
 	// Choose a word for user to guess
-	chosenWord = wordList[Math.floor(Math.random() * wordList.length)];
+	currentWord = wordList[Math.floor(Math.random() * wordList.length)];
 
-	console.log(chosenWord);
+	console.log(currentWord);
 
 	// For Debug Only
-	//console.log(chosenWord);
-	console.log("> " + chosenWord.length);
+	//console.log(currentWord);
+	//console.log("> " + currentWord.length);
 
 	// Since this is a new word, setup the display div
-	wordToGuessDiv.textContent = '';
+	updateElementTextContent(wordToGuessDiv, '', false);
 
-	for(var i=0; i<chosenWord.length; i++) {
+	// Clear the word guess progress array
+	wordGuessProgress = [];
+
+	for(var i=0; i<currentWord.length; i++) {
 		wordGuessProgress.push("_");
 	}
+
+	// Set the total to win value
+	totalToWin = currentWord.length;
 
 	return;
 }
@@ -92,9 +127,9 @@ function setupNewWord() {
 function displayWordGuessProgress() {
 	for(var i=0; i<wordGuessProgress.length; i++) {
 		if(i === 0) {
-			wordToGuessDiv.textContent = wordGuessProgress[i] + '    '
+			updateElementTextContent(wordToGuessDiv, wordGuessProgress[i] + '    ', false);
 		} else {
-			wordToGuessDiv.textContent += wordGuessProgress[i] + '    '
+			updateElementTextContent(wordToGuessDiv, wordGuessProgress[i] + '    ', true);
 		}
 	}
 
@@ -102,19 +137,72 @@ function displayWordGuessProgress() {
 }
 
 function updateUserGuesses(userGuess) {
-	userGuessesDiv.textContent += ' ' + userGuess;
+	updateElementTextContent(userGuessListDiv, ' ' + userGuess, true);
 
 	return;
 }
 
 function clearUserGuesses() {
-	userGuessesDiv.textContent = '';
+	updateElementTextContent(userGuessListDiv, '', false);
+
+	return;
 }
 
 function checkUserGuess(userGuess) {
 	// Check if userGuess is contained within the current word
-	for(var i=0; i<chosenWord.length; i++) {
+	var indexOfGuess = currentWord.toLowerCase().indexOf(userGuess);
 
+	// Debug ONLY
+	//console.log("indexOfGuess", indexOfGuess);
+
+	while(indexOfGuess >= 0) {
+		totalCorrect++;
+
+		wordGuessProgress[indexOfGuess] = userGuess;
+
+		indexOfGuess = currentWord.toLowerCase().indexOf(userGuess, indexOfGuess + 1);
+
+		// Debug ONLY
+		//console.log("indexOfGuess", indexOfGuess);
+	}
+
+	return;
+}
+
+function updateNumberOfGuessesRemaining(resetGuesses) {
+	if(resetGuesses) {
+		numberOfGuessesRemaining = 15;
+	}
+	else {
+		numberOfGuessesRemaining--;
+	}
+
+	updateElementTextContent(guessesRemainingDiv, numberOfGuessesRemaining, false);
+
+	return;
+}
+
+function isWinner() {
+	if(totalCorrect === totalToWin) {
+		return true;
+	}
+
+	return false;
+}
+
+function moreGuessesAllowed() {
+	if(numberOfGuessesRemaining >= 1) {
+		return true;
+	} 
+
+	return false;
+}
+
+function updateElementTextContent(element, text, append) {
+	if(append) {
+		element.textContent += text;
+	} else {
+		element.textContent = text;
 	}
 
 	return;
